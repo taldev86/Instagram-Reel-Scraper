2ba8273548ebdcef5015b24185aeb41848db580c
Binh Bui <bvbinh@sk-global.biz>
2024-01-19 10:33:39 +0700

    Add instructions for generating long lived access token

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Instagram Reels Bot
 
 ## What does Instagram Reel Scraper Bot do?
+
+
+## How to generate the Long Lived Access Token?
+
+1. Create a short lived access token from [Facebook Graph API Explorer](https://developers.facebook.com/tools/explorer/).
+2. Get app id and app secret from [Facebook for Developers](https://developers.facebook.com/apps/).
+3. Update the `appId`, `appSecret` and `accessToken` in `scripts/gen_token.js` file.
+4. Run `npm run gen_token` to generate the long lived access token.
+
--- scripts/gen_token.js
+++ scripts/gen_token.js
@@ -5,6 +5,7 @@ const run = async () => {
   console.log('====STARTING====');
   // short-lived access token can be generated here: https://developers.facebook.com/tools/explorer/
   // app id and app secret can be found in your app stings: https://developers.facebook.com/apps/
+  // you check the expiration date of your token here: https://developers.facebook.com/tools/debug/accesstoken/
   const accessToken = await generateLongLivedAccessToken({
     accessToken: "put your short-lived access token here",
     appId: "put your app id here",
